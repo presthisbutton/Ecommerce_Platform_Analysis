5b31d90ac4198efb29c94d2e2efdb546a478a79b
presthisbutton <100450845+presthisbutton@users.noreply.github.com>
2022-05-05 10:30:28 -0400

    Edited business questions

--- Business_Questions.sql
+++ Business_Questions.sql
@@ -1,51 +1,53 @@
 USE magist;
 
-/* In relation to the products:
-What categories of tech products does Magist have?
-How many products of these tech categories have been sold (within the time window of the database snapshot)?
-What percentage does that represent from the overall number of products sold?
-What’s the average price of the products being sold?
-Are expensive tech products popular? */
+# In relation to the products:
 
+# What categories of tech products does Magist have?
 SELECT product_category_name, COUNT(*)
 FROM products
-WHERE product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
-GROUP BY product_category_name;
+WHERE product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs", "tablets_impressao_imagem")
+GROUP BY product_category_name
+ORDER BY COUNT(*) DESC;
 
+SELECT COUNT(product_id)
+FROM products;
 
-SELECT products.product_category_name, COUNT(order_items.product_id) AS num_items
+# How many products of these tech categories have been sold (within the time window of the database snapshot)?
+# What percentage does that represent from the overall number of products sold?
+SELECT products.product_category_name, COUNT(order_items.product_id) AS num_items_sold
 FROM order_items
-INNER JOIN products
+LEFT JOIN products
 ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
-GROUP BY products.product_category_name;
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs", "tablets_impressao_imagem")
+GROUP BY products.product_category_name
+ORDER BY COUNT(order_items.product_id) DESC;
 
-SELECT SUM(num_items)
+SELECT SUM(num_items_sold)
 FROM (
-	SELECT products.product_category_name, COUNT(*) AS num_items
+	SELECT products.product_category_name, COUNT(*) AS num_items_sold
 	FROM order_items
-I	INNER JOIN products
+	LEFT JOIN products
 	ON order_items.product_id = products.product_id
-	WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-	"pc_gamer", "pcs")
+	WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+    "pc_gamer", "pcs", "tablets_impressao_imagem")
 	GROUP BY products.product_category_name
 ) AS tech_sales;
 
 SELECT COUNT(product_id)
 FROM order_items;
 
-
+# What’s the average price of the products being sold?
 SELECT products.product_category_name, ROUND(AVG(order_items.price), 2)
 FROM order_items
 LEFT JOIN products
 ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+    "pc_gamer", "pcs", "tablets_impressao_imagem")
 GROUP BY products.product_category_name;
 
-
+# Are expensive tech products popular?
 SELECT products.product_category_name, order_items.price,
 	CASE
 		WHEN price <= 100 THEN "very cheap"
@@ -56,15 +58,15 @@ SELECT products.product_category_name, order_items.price,
 FROM order_items
 LEFT JOIN products
 ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+    "pc_gamer", "pcs", "tablets_impressao_imagem")
 ORDER BY order_items.price DESC;
 
-SELECT COUNT(*)
+SELECT price_category, COUNT(*)
 FROM (
 SELECT products.product_category_name, order_items.price,
-	CASE
-		WHEN price <= 100 THEN "very cheap"
+    CASE
+        WHEN price <= 100 THEN "very cheap"
         WHEN price <= 300 THEN "cheap"
         WHEN price <= 1000 THEN "moderate"
         WHEN price > 1000 THEN "expensive"
@@ -72,36 +74,29 @@ SELECT products.product_category_name, order_items.price,
 FROM order_items
 LEFT JOIN products
 ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+    "pc_gamer", "pcs", "tablets_impressao_imagem")
 ORDER BY order_items.price
 ) AS sales_table
-WHERE price_category = "expensive";
+GROUP BY price_category
+ORDER BY COUNT(*) DESC;
 
-SELECT COUNT(*)
+SELECT COUNT(order_items.price)
 FROM order_items
 LEFT JOIN products
 ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs");
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs", "tablets_impressao_imagem")
+AND order_items.price > 1000;
 
-SELECT products.product_category_name, COUNT(order_items.price)
-FROM order_items
-LEFT JOIN products
-ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
-AND order_items.price > 1000
-GROUP BY products.product_category_name;
 
-/* In relation to the sellers:
-How many sellers are there?
-What’s the average monthly revenue of Magist’s sellers?
-What’s the average revenue of sellers that sell tech products? */
-SELECT COUNT(*)
+# In relation to the sellers:
+
+# How many sellers are there?
+SELECT DISTINCT COUNT(*)
 FROM sellers;
 
-
+# What’s the average monthly revenue of Magist’s sellers?
 SELECT seller_id, ROUND(AVG(total_rev), 2) AS avg_rev
 FROM (
 SELECT sellers.seller_id, YEAR(order_items.shipping_limit_date) AS year,
@@ -115,7 +110,15 @@ ORDER BY year, month
 GROUP BY seller_id
 ORDER BY avg_rev DESC;
 
+SELECT ROUND(AVG(total_rev), 2) AS avg_rev
+FROM (
+SELECT order_items.seller_id, YEAR(order_items.shipping_limit_date) AS year, 
+MONTH(order_items.shipping_limit_date) AS month, ROUND(SUM(order_items.price), 2) AS total_rev
+FROM order_items
+GROUP BY seller_id, year, month
+) AS avg_monthly_rev;
 
+# What’s the average revenue of sellers that sell tech products?
 SELECT seller_id, ROUND(AVG(total_rev), 2) AS avg_rev
 FROM (
 SELECT sellers.seller_id, YEAR(order_items.shipping_limit_date) AS year,
@@ -125,21 +128,34 @@ LEFT JOIN order_items
 ON sellers.seller_id = order_items.seller_id
 LEFT JOIN products
 ON order_items.product_id = products.product_id
-WHERE products.product_category_name IN ("audio", "consoles_games", "electronicos", "informatica_acessorios",
-"pc_gamer", "pcs")
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs", "tablets_impressao_imagem")
 GROUP BY sellers.seller_id, year, month
 ORDER BY year, month
 ) AS monthly_rev
 GROUP BY seller_id
 ORDER BY avg_rev DESC;
 
-/* In relation to the delivery time:
-What’s the average time between the order being placed and the product being delivered?
-How many orders are delivered on time vs orders delivered with a delay?
-Is there any pattern for delayed orders, e.g. big products being delayed more often? */
+SELECT ROUND(AVG(total_rev), 2) AS avg_rev
+FROM (
+SELECT order_items.seller_id, YEAR(order_items.shipping_limit_date) AS year, 
+MONTH(order_items.shipping_limit_date) AS month, ROUND(SUM(order_items.price), 2) AS total_rev
+FROM order_items
+LEFT JOIN products
+ON order_items.product_id = products.product_id
+WHERE products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs", "tablets_impressao_imagem")
+GROUP BY seller_id, year, month
+) AS avg_monthly_rev;
+
+
+# In relation to the delivery time:
+
+# What’s the average time between the order being placed and the product being delivered?
 SELECT AVG(DATEDIFF(order_delivered_customer_date, order_purchase_timestamp)) AS avg_del_time
 FROM orders;
 
+# How many orders are delivered on time vs orders delivered with a delay?
 SELECT order_id, order_status, order_delivered_customer_date, order_estimated_delivery_date
 FROM orders
 WHERE order_status = "delivered"
@@ -160,7 +176,7 @@ FROM orders
 WHERE order_status = "delivered"
 AND DATEDIFF(order_delivered_customer_date, order_estimated_delivery_date) > 0;
 
-
+# Is there any pattern for delayed orders, e.g. big products being delayed more often?
 SELECT orders.order_id, order_payments.payment_value AS amount
 FROM orders
 LEFT JOIN order_payments
@@ -189,7 +205,36 @@ FROM orders
 LEFT JOIN order_payments
 ON orders.order_id = order_payments.order_id
 WHERE orders.order_status = "delivered"
+AND DATEDIFF(orders.order_delivered_customer_date, orders.order_estimated_delivery_date) > 0;
+
+SELECT products.product_category_name, ROUND(AVG(order_items.price), 2) AS avg_price
+FROM orders
+LEFT JOIN order_items
+ON orders.order_id = order_items.order_id
+LEFT JOIN products
+ON order_items.product_id = products.product_id
+WHERE orders.order_status = "delivered"
+AND DATEDIFF(orders.order_delivered_customer_date, orders.order_estimated_delivery_date) <= 0
+AND products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs")
+GROUP BY products.product_category_name;
+
+SELECT products.product_category_name, ROUND(AVG(order_items.price), 2) AS avg_price
+FROM orders
+LEFT JOIN order_items
+ON orders.order_id = order_items.order_id
+LEFT JOIN products
+ON order_items.product_id = products.product_id
+WHERE orders.order_status = "delivered"
 AND DATEDIFF(orders.order_delivered_customer_date, orders.order_estimated_delivery_date) > 0
+AND products.product_category_name IN ("audio", "consoles_games", "eletronicos", "informatica_acessorios",
+"pc_gamer", "pcs")
+GROUP BY products.product_category_name;
+
+
+
+
+
 
 
 
